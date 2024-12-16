df82cd19d68c6f873445356d5149f1af32ebc0a9
wangkeming <376086637@qq.com>
2024-12-16 20:47:51 +0800

    feat:个人博客 init

---
+++ src/main/java/cn/good/yx/YxApplication.java
@@ -0,0 +1,13 @@
+package cn.good.yx;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YxApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YxApplication.class, args);
+    }
+
+}
---
+++ src/test/java/cn/good/yx/YxApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.good.yx;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YxApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
